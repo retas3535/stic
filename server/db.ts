51e6e970ba0a15b131ffb4b97369302c3373d0f3
retasaltyapi <teknik@retasaltyapi.com>
2025-07-29 02:01:13 +0300

    3

--- server/db.ts
+++ server/db.ts
@@ -1,17 +1,11 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
+import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
 import * as schema from "@shared/schema";
 
-// Neon için WebSocket yapılandırması
-neonConfig.webSocketConstructor = ws;
+// WebSocket desteği kaldırıldı
 
-// DATABASE bağlantı URL’si (External URL)
-const DATABASE_URL = "postgresql://etbsdatabase_user:UtCvKY7GwRI4BOfvyGZDsy0QKmGyJCsL@dpg-d1unan6mcj7s73emgru0-a.oregon-postgres.render.com/etbsdatabase";
+const DATABASE_URL = "postgresql://etbsdatabase_user:UtCvKY7GwRI4BOfvyGZDsy0QKmGyJCsL@dpg-d1unan6mcj7s73emgru0-a/etbsdatabase";
 
-// Pool üzerinden bağlantı oluşturuluyor
 export const pool = new Pool({ connectionString: DATABASE_URL });
 
-// Drizzle ORM ile bağlanıyoruz
 export const db = drizzle(pool, { schema });
-
