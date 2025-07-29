dffbf218fe623e65c832669cb3d1e77ecdd49ca3
retasaltyapi <teknik@retasaltyapi.com>
2025-07-29 08:23:53 +0300

    1

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,15 @@
-import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from "ws";
 import * as schema from "@shared/schema";
 
-// Internal URL ile bağlantı
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+neonConfig.webSocketConstructor = ws;
 
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
+}
+
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
