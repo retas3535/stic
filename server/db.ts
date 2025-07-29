f697c5c14465e828c4d1381bf2ea0134c0915569
retasaltyapi <teknik@retasaltyapi.com>
2025-07-29 23:07:23 +0300

    23

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,9 @@
+
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
+
 
 neonConfig.webSocketConstructor = ws;
 
